30dee4d18383cd6f76a51fae990d2ef1b8f9ed17
Martin Guillon <martin.guillon@akylas.fr>
2021-12-26 17:21:16 +0100

    chore: update build script

--- make-bridge-loader.ts
+++ make-bridge-loader.ts
@@ -10,15 +10,28 @@ async function nativescriptWebviewBridgeLoader(platform: 'ios' | 'android') {
 
     const values = {
         // fetchPolyfill: await fsReadFile('./node_modules/whatwg-fetch/dist/fetch.umd.js', 'UTF-8'),
-        promisePolyfill: await fsReadFile('./www-src/node_modules/promise-polyfill/dist/polyfill.js', 'UTF-8'),
+        // promisePolyfill: await fsReadFile('./www-src/node_modules/promise-polyfill/dist/polyfill.js', 'UTF-8'),
         webViewBridge: await fsReadFile(`./build/bridge.${platform}.js`, 'UTF-8'),
         metadataViewPort: await fsReadFile('./build/metadata-view-port.js', 'UTF-8'),
     };
 
     for (const [name, value] of Object.entries(values)) {
         const terserRes = await Terser.minify(value, {
-            compress: true,
-            mangle: false,
+            ecma: 2019,
+            module: false,
+            toplevel: false,
+            keep_classnames: false,
+            keep_fnames: false,
+            compress: {
+                passes: 2,
+                drop_console: true
+            },
+            mangle: {
+                toplevel: true,
+                properties: {
+                    regex: /^(m[A-Z])/
+                }
+            }
         });
         template = template.replace(`<?= ${name} ?>`, JSON.stringify(terserRes.code));
     }
