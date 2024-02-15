8e3af59f5ffab54deae4a4bd4b371e4edef322f3
Martin Guillon <dev@akylas.fr>
2024-02-15 15:29:09 +0100

    chore: demo

--- demo-snippets/svelte/install.ts
+++ demo-snippets/svelte/install.ts
@@ -1,5 +1,8 @@
 import BasicExample from './BasicExample.svelte';
+import installWebRTC from '@nativescript-community/ui-webview-rtc';
 
-export function installPlugin() {}
+export function installPlugin() {
+    installWebRTC();
+}
 
 export const demos = [{ name: 'Basic Example', path: 'basic', component: BasicExample }];
--- demo-snippets/vue/install.ts
+++ demo-snippets/vue/install.ts
@@ -2,12 +2,14 @@ import Vue from 'nativescript-vue';
 import WebView from '@nativescript-community/ui-webview/vue';
 import {WebViewTraceCategory} from '@nativescript-community/ui-webview';
 import {Trace} from '@nativescript/core';
+import installWebRTC from '@nativescript-community/ui-webview-rtc';
 
 import BasicExample from './BasicExample.vue';
 
 export function installPlugin() {
     Trace.addCategories(WebViewTraceCategory);
     Trace.enable();
+    installWebRTC();
     Vue.use(WebView);
 }
 
